1f1a859307633dffb9cb3e9e1d1230bc6e7ae3b4
Ezekiel <ezekielfaulknor@gmail.com>
2021-09-13 12:19:24 -0400

    Unit2

---
+++ filxster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  filxster
+//
+//  Created by Ezekiel Faulknor on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
